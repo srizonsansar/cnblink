65e7881f95909122bd417daf16017086694f2088
Hem Raj <hemrajbhatta.pt@gmail.com>
2022-09-04 12:41:22 +0100

    services section done home page

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-<?php include('_header.php'); ?>
+<?php
+  include('_header.php');
 
- <h1>hello there</h1>
-
- <svg class="icon">
-  <use xlink:href="#icon-web-development"></use>
-</svg>
-<svg class="icon">
-  <use xlink:href="#icon-ecommerce-services"></use>
-</svg>
-
-
-
-<?php include('_footer.php'); ?>
+  // Services Section
+  include('_services.php');
+  
+  include('_footer.php');
+?>
